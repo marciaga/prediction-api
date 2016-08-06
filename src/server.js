f9678da4ecd5bd54193ab87677673a321d15b389
marciaga <mark.arciaga@gmail.com>
2016-08-06 10:50:59 -0700

    Switch to modifying main manifest.json

--- src/server.js
+++ src/server.js
@@ -5,8 +5,14 @@ require('babel-core/register');
 const path = require('path');
 const Glue = require('glue');
 const options = { relativeTo: __dirname };
+const url = process.env.DB_CONNECTION;
 
-const manifest = process.env.NODE_ENV === 'production' ? require('./config/manifest.production.js') : require('./config/manifest.json');
+let manifest = require('./config/manifest.json');
+
+if (process.env.NODE_ENV === 'production') {
+    let prodMongo = manifest.registrations.find(r => r.plugin.register && r.plugin.register === 'hapi-mongodb');
+    prodMongo.plugin.options.url = url;
+}
 
 Glue.compose(manifest, options, (err, server) => {
     server.start((err) => {
